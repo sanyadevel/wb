3763240f1a238bc6c5892ccb51f9ab30cda66137
andrejsamyckov <Shamandrey@gmail.com>
2023-08-02 09:02:41 +0300

    env

--- src/pages/root/_components/page-view/index.tsx
+++ src/pages/root/_components/page-view/index.tsx
@@ -2,7 +2,7 @@ import React, { memo } from 'react';
 import { RootContent } from 'src/layouts';
 
 export const PageView = memo(() => {
-  return <RootContent />;
+  return <RootContent>INIT</RootContent>;
 });
 
 export default PageView;
